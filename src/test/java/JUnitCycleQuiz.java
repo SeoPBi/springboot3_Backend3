0bee88f11964c655d1168086da72a36cdec024e6
sinh8 <aktms252@naver.com>
2025-03-21 12:57:50 +0900

    25-03-21

---
+++ src/test/java/JUnitCycleQuiz.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class JUnitCycleQuiz {
+
+    @Test
+    public void junitQuiz3(){
+        System.out.println("This is first test");
+    }
+
+    @Test
+    public void junitQuiz4(){
+        System.out.println("This is second test");
+    }
+}
---
+++ src/test/java/me/shinhyeonseop/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,26 @@
+package me.shinhyeonseop.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void saveMember() {
+        // given
+        Member member = new Member(1L, "A");
+
+        // when
+        memberRepository.save(member);
+
+        // then
+        assertThat(memberRepository.findByid(1L).get().getName()).isEqualTo("A");
+    }
+}
